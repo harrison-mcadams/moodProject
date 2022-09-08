ff9b74f74162f38864da239af9c4e1b6eb285317
Harrison McAdams <harrison.mcadams@gmail.com>
2022-09-08 09:46:06 -0400

    more keywords to play with

--- python/analyzeExercise.py
+++ python/analyzeExercise.py
@@ -2,11 +2,18 @@ import compare, getMoodData
 
 email='harrison.mcadams@gmail.com'
 year=2020
-csvPath = '/var/www/html/login'
+csvPath = '/Users/harrisonmcadams/Desktop'
+savePath = '/Users/harrisonmcadams/Desktop'
 
 moodData = getMoodData.getMoodData(email, csvPath)
 
 comparisonAKeyWords = ['exercise', 'worked out', 'working out', 'work out', 'workout', 'run', 'yoga', 'madfit', 'abs']
-comparisonBKeyWords = ['not working out', 'not work out', 'no exercise', 'not exercise', 'REMAINDER']
+comparisonBKeyWords = ['not working out', 'not work out', 'no exercise', 'not exercise', 'skipped working out', 'REMAINDER']
+
+comparisonAKeyWords = ['productive']
+comparisonBKeyWords = ['unproductive', 'not very productive', 'didn\'t feel particularly productive', 'not at all productive', 'wasn\'t able to be productive', 'not really productive']
+
+comparisonAKeyWords = ['call']
+comparisonBKeyWords = ['boring']
 
 compare.compare(moodData, comparisonAKeyWords, comparisonBKeyWords)
